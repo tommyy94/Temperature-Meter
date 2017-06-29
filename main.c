5f164fa2e3562f1d4b3b15bf013b89eff7d21963
Tommy <tommyy94@users.noreply.github.com>
2017-06-29 23:28:47 +0300

    Update main.c
    - Calculating temperature values are now done inside function sensor_read()
    - Pass by reference instead of pass by value

--- main.c
+++ main.c
@@ -1,60 +1,62 @@
-/***************************************************************************
-*
-*   This file is part of WeatherStation.
-*
-*   WeatherStation is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   WeatherStation is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with LCD_library. If not, see <http://www.gnu.org/licenses/>.
-*
-*   Copyright (C) 2017 by tommyy94
-*
-***************************************************************************/
+/******************************************************************************
+*                                                                             *
+*   This file is part of TemperatureMeter.	                              *
+*                                                                             *
+*   TemperatureMeter is free software: you can redistribute it and/or         *
+*   modify it under the terms of the GNU General Public License as            *
+*   published by the Free Software Foundation, either version 3 of the        *
+*   License, or (at your option) any later version.                           *
+*                                                                             *
+*   TemperatureMeter is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*   GNU General Public License for more details.                              *
+*                                                                             *
+*   You should have received a copy of the GNU General Public License along   *
+*   with TemperatureMeter If not, see <http://www.gnu.org/licenses/>.         *
+*                                                                             *
+*   Copyright (C) 2017 by tommyy94                                            *
+*                                                                             *
+******************************************************************************/
 
-/***************************************************************************
+/******************************************************************************
 *
 * 1. NAME
-*		main.c
+*       main.c
 *
 * 2. DESCRIPTION
-*		Main source
+*       Main file
 *
-***************************************************************************/
+******************************************************************************/
 
-/***************************************************************************
-*   HEADER FILES
-***************************************************************************/
+/******************************************************************************
+*   HEADER FILES                                                              *
+******************************************************************************/
 #include "main.h"
 
-/***************************************************************************
-*	MAIN PROGRAM
-***************************************************************************/
+/******************************************************************************
+*   MAIN PROGRAM                                                              *
+******************************************************************************/
 int main(void)
 {
-	struct Sensors Temp;
+    struct sensor temperature;
+    struct sensor *const ptr_temperature = &temperature;
+    int8_t *const ptr_celsius = &temperature.celsius;
+    int8_t *const ptr_fahrenheit = &temperature.fahrenheit;
+    
+    adc_init();
+    lcd_init();
 
-	adc_init(PINC0);
-	lcd_init();
-
-	while(1)
-	{
-		Temp.celsius = sensor_read(Temp, TEMP_SENSOR)/2;
-		Temp.fahrenheit = (Temp.celsius * (9*10)/5)/10 + 32;
-
-		lcd_send_string(0, 0, "Celsius: ");
-		lcd_send_int(DIGIT_POSITION, 0, Temp.celsius);
-		lcd_send_string(0, 1, "Fahrenheit: ");
-		lcd_send_int(DIGIT_POSITION, 1, Temp.fahrenheit);
-
-		_delay_ms(DELAY);
-	}
-	return 0;
-} /* end of main */
+    while(1)
+    {
+        sensor_read(ptr_temperature);
+        
+        lcd_send_string(0, 0, "Celsius");
+        lcd_send_int(DIGIT_POSITION, 0, ptr_celsius);
+        lcd_send_string(0, 1, "Fahrenheit");
+        lcd_send_int(DIGIT_POSITION, 1, ptr_fahrenheit);
+        
+        _delay_ms(DELAY);
+    }
+    return 0;
+}
