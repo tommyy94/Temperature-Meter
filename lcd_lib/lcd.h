45908881ca39cb5d6b283bc5265ea7161ca643f6
Tommy <tommyy94@users.noreply.github.com>
2017-06-29 23:50:20 +0300

    Update lcd.h
    - Fixed typos on macros
    - Moved static function prototypes to source file
    - Gave some static functions more descriptive names:
        - lcd_check_if_busy() => lcd_wait_if_busy()
        - lcd_keep_enabled() => lcd_execute_instruction()

--- lcd_lib/lcd.h
+++ lcd_lib/lcd.h
@@ -1,33 +1,33 @@
-/***************************************************************************
-*
-*   This file is part of LCD_library.
-*
-*   LCD_library is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   LCD_library is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with LCD_library. If not, see <http://www.gnu.org/licenses/>.
-*
-*	Copyright (C) 2017 by tommyy94
-*
-***************************************************************************/
+/******************************************************************************
+*                                                                             *
+*   This file is part of TemperatureMeter.                                    *
+*                                                                             *
+*   TemperatureMeter is free software: you can redistribute it and/or         *
+*   modify it under the terms of the GNU General Public License as            *
+*   published by the Free Software Foundation, either version 3 of the        *
+*   License, or (at your option) any later version.                           *
+*                                                                             *
+*   TemperatureMeter is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*   GNU General Public License for more details.                              *
+*                                                                             *
+*   You should have received a copy of the GNU General Public License along   *
+*   with TemperatureMeter If not, see <http://www.gnu.org/licenses/>.         *
+*                                                                             *
+*   Copyright (C) 2017 by tommyy94                                            *
+*                                                                             *
+******************************************************************************/
 
-/***************************************************************************
+/******************************************************************************
 *
 * 1. NAME
-*		lcd.h
+*       lcd.h
 *
 * 2. DESCRIPTION
-*		LCD header
 *
-***************************************************************************/
+*
+******************************************************************************/
 #ifndef LCD_H_
 #define LCD_H_
 
@@ -35,46 +35,53 @@
 #define F_CPU 1000000UL
 #endif
 
-/***************************************************************************
-*   HEADER FILES
-***************************************************************************/
+/******************************************************************************
+*   HEADER FILES                                                              *
+******************************************************************************/
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
 
-/***************************************************************************
-*   GLOBAL VARIABLES
-***************************************************************************/
+/******************************************************************************
+*   GLOBAL VARIABLES                                                          *
+******************************************************************************/
 /* Control flags */
 
 /* Global constants */
 #define EN PINB0
 #define RW PINB3
 #define RS PINB2
+#define DATA_LINES PORTD
+#define CONTROL_LINES PORTB
+#define DATA_DIRECTION DDRD
+#define CONTROL_DIRECTION DDRB
+
+#define ROWS 2
+#define COLUMNS 16
 
 #define CLEAR_DISPLAY 0x01
 #define SET_4_BIT 0x28
 #define SET_8_BIT 0x38
-#define DISPLAY_ON_CURSON_OFF 0x0C
-#define DISPLAY_ON_CURSON_ON 0x0E
-#define DISPLAY_ON_CURSON_BLINK 0x0F
+#define DISPLAY_ON_CURSOR_OFF 0x0C
+#define DISPLAY_ON_CURSOR_ON 0x0E
+#define DISPLAY_ON_CURSOR_BLINK 0x0F
 #define FIRST_LINE 0x80
 #define SECOND_LINE 0xC0
+
+#ifndef NOP
+#define NOP asm volatile("nop")
+#endif
 
 /* Global variables */
 
 /* Global structures */
 
-/****************************************************************************
-*   FUNCTION PROTOTYPES
-****************************************************************************/
-void lcd_check_if_busy(void);
-void lcd_keep_enabled(void);
-void lcd_send_command(unsigned char command);
-void lcd_goto_xy(uint8_t x, uint8_t y);
-void lcd_send_character(unsigned char character);
-void lcd_send_string(uint8_t x, uint8_t y, char *string);
-void lcd_send_int(uint8_t x, uint8_t y, int int_to_display, unsigned char number_of_digits);
+/******************************************************************************
+*   FUNCTION PROTOTYPES                                                       *
+******************************************************************************/
+void lcd_send_character(const char character);
+void lcd_send_string(const uint8_t x, const uint8_t y, char *string);
+void lcd_send_int(const uint8_t x, const uint8_t y, int8_t *int_to_display);
 void lcd_init(void);
 
 #endif /* LCD_H_ */
