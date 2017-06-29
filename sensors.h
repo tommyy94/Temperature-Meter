0f4cca4b09713dee80f2305906f3ad0e93d8fbd0
Tommy <tommyy94@users.noreply.github.com>
2017-06-29 23:38:51 +0300

    Update sensors.h
    - Struct members are signed instead of unsigned
    - sensor_read is void, pointer to struct as parameter

--- sensors.h
+++ sensors.h
@@ -1,70 +1,61 @@
-/***************************************************************************
-*
-*   This file is part of WeatherStation.
-*
-*   WeatherStation is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   WeatherStation is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with LCD_library. If not, see <http://www.gnu.org/licenses/>.
-*
-*	Copyright (C) 2017 by tommyy94
-*
-***************************************************************************/
+/******************************************************************************
+*                                                                             *
+*   This file is part of TemperatureMeter.                                    *
+*                                                                             *
+*   TemperatureMeter is free software: you can redistribute it and/or         *
+*   modify it under the terms of the GNU General Public License as            *
+*   published by the Free Software Foundation, either version 3 of the        *
+*   License, or (at your option) any later version.                           *
+*                                                                             *
+*   TemperatureMeter is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*   GNU General Public License for more details.                              *
+*                                                                             *
+*   You should have received a copy of the GNU General Public License along   *
+*   with TemperatureMeter If not, see <http://www.gnu.org/licenses/>.         *
+*                                                                             *
+*   Copyright (C) 2017 by tommyy94                                            *
+*                                                                             *
+******************************************************************************/
 
-/***************************************************************************
+/******************************************************************************
 *
 * 1. NAME
-*		sensors.h
+*       sensor
 *
 * 2. DESCRIPTION
-*		Sensor header
 *
-***************************************************************************/
+*
+******************************************************************************/
+#ifndef SENSOR_H_
+#define SENSOR_H_
 
-#ifndef SENSORS_H_
-#define SENSORS_H_
-
-#ifndef F_CPU
-#define F_CPU 1000000UL
-#endif
-
-/***************************************************************************
-*   HEADER FILES
-***************************************************************************/
-#include <util/delay.h>
+/******************************************************************************
+*   HEADER FILES                                                              *
+******************************************************************************/
 #include <stdint.h>
 #include "adc_lib/adc.h"
 
-/***************************************************************************
-*   GLOBAL VARIABLES
-***************************************************************************/
+/******************************************************************************
+*   GLOBAL VARIABLES                                                          *
+******************************************************************************/
 /* Control flags */
-#define TEMP_SENSOR 1
 
 /* Global constants */
-#define SAMPLES 10
 
 /* Global variables */
 
 /* Global structures */
-struct Sensors
+struct sensor
 {
-	uint8_t celsius;
-	uint8_t fahrenheit;
-	uint8_t value; /* change to an array */
+    int8_t celsius;
+    int8_t fahrenheit;
 };
 
-/****************************************************************************
-*   FUNCTION PROTOTYPES
-****************************************************************************/
-uint8_t sensor_read(struct Sensors, uint8_t sensor_type);
+/******************************************************************************
+*   FUNCTION PROTOTYPES                                                       *
+******************************************************************************/
+void sensor_read(struct sensor *temperature);
 
-#endif /* SENSORS_H_ */
+#endif /* SENSOR_H_ */
