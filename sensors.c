af0d62e68a30204c700cfecb330f66d2de8b418e
Tommy <tommyy94@users.noreply.github.com>
2017-06-29 23:35:46 +0300

    Update sensors.c
    - Removed useless switch-case
    - Calculating temperature values is now done here
    - sensor_read() is given analog channel to read from
    - Instead of returning saves value to struct with pointer

--- sensors.c
+++ sensors.c
@@ -1,62 +1,55 @@
-/***************************************************************************
-*
-*   This file is part of WeatherStation.
-*
-*   WeatherStation is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   WeatherStation is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with LCD_library. If not, see <http://www.gnu.org/licenses/>.
-*
-*	Copyright (C) 2017 by tommyy94
-*
-***************************************************************************/
+/******************************************************************************
+*                                                                             *
+*   This file is part of TemperatureMeter.                                    *
+*                                                                             *
+*   TemperatureMeter is free software: you can redistribute it and/or         *
+*   modify it under the terms of the GNU General Public License as            *
+*   published by the Free Software Foundation, either version 3 of the        *
+*   License, or (at your option) any later version.                           *
+*                                                                             *
+*   TemperatureMeter is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*   GNU General Public License for more details.                              *
+*                                                                             *
+*   You should have received a copy of the GNU General Public License along   *
+*   with TemperatureMeter If not, see <http://www.gnu.org/licenses/>.         *
+*                                                                             *
+*   Copyright (C) 2017 by tommyy94                                            *
+*                                                                             *
+******************************************************************************/
 
-/***************************************************************************
+/******************************************************************************
 *
 * 1. NAME
-*		sensors.c
+*       sensor.c
 *
 * 2. DESCRIPTION
+*       Reads sensor values and calculates them.
 *
-*
-***************************************************************************/
+******************************************************************************/
 
-/***************************************************************************
-*   HEADER FILES
-***************************************************************************/
-#include "sensors.h"
+/******************************************************************************
+*   HEADER FILES                                                              *
+******************************************************************************/
+#include "sensor.h"
 
-/****************************************************************************
-*   FUNCTION PROTOTYPES
-****************************************************************************/
+/******************************************************************************
+*   FUNCTION PROTOTYPES                                                       *
+******************************************************************************/
 
-/***************************************************************************
-;	F U N C T I O N    D E S C R I P T I O N
-;---------------------------------------------------------------------------
-; NAME: sensor_read
-; DESCRIPTION:
-; Input: struct Sensors, sensors type
-; Output: Current temperature value
-; Used global variables: -
-; REMARKS when using this function: sensor_types: TEMP_SENSOR
-;**************************************************************************/
-uint8_t sensor_read(struct Sensors Object, uint8_t sensor_type)
+/******************************************************************************
+*   FUNCTION: sensor_read                                                     *
+***************************************************************************//**
+@brief Read adc values and compute corresponding temperature values.
+@param Pointer to struct sensor
+******************************************************************************/
+void sensor_read(struct sensor *temperature)
 {
-	switch(sensor_type)
-	{
-		case TEMP_SENSOR:
-			Object.value = adc_read();
-			break;
-		default:
-			break;
-	}
-	return Object.value;
-} /* end of sensor_read */
+    temperature->celsius = ((adc_read(PINC0) * 5 / 1024.0) - 0.5) * 100;
+    temperature->fahrenheit = temperature->celsius * 9 / 5 + 32;
+    /**
+     * @todo figure way to add decimal without floating point
+     */
+    return;
+}
