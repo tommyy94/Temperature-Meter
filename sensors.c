0efde2c4f5a1ff0045da941a7fb90fc54049dfc5
Tommy <tommyy94@users.noreply.github.com>
2017-09-23 00:00:44 +0300

    Added initializer function, now calculates average

--- sensors.c
+++ sensors.c
@@ -1,7 +1,7 @@
 /******************************************************************************
 *
 * 1. NAME
-*       sensor.c
+*       sensors.c
 *
 * 2. DESCRIPTION
 *       Reads sensor values and calculates them.
@@ -16,6 +16,46 @@
 /******************************************************************************
 *   FUNCTION PROTOTYPES                                                       *
 ******************************************************************************/
+static int32_t calculate_average(const uint8_t analog_channel);
+
+/******************************************************************************
+*   FUNCTION: sensor_init                                                     *
+***************************************************************************//**
+@brief
+@param
+******************************************************************************/
+void sensor_init(const uint8_t analog_channel)
+{
+    uint8_t i;
+    
+    for(i = 0; i < SAMPLES; i++) {
+        adc_read(analog_channel);
+        _delay_ms(10);
+    }
+}
+
+/******************************************************************************
+*   FUNCTION: calculate_average                                               *
+***************************************************************************//**
+@brief
+@param
+******************************************************************************/
+static int32_t calculate_average(const uint8_t analog_channel)
+{
+    int32_t average = 0;
+    uint8_t i;
+    /* uint8_t *ptr_average = &average; */
+    /*
+     * @TODO: return pointer to "average"
+     */
+    
+    for(i = 0; i < SAMPLES; i++) {
+        average += adc_read(analog_channel);
+        _delay_ms(10);
+    }
+    average /= SAMPLES;
+    return average;
+}
 
 /******************************************************************************
 *   FUNCTION: sensor_read                                                     *
@@ -25,10 +65,10 @@
 ******************************************************************************/
 void sensor_read(struct sensor *temperature)
 {
-    int32_t celsius, fahrenheit;
     const char decimal_mark[] = ".";
+    int32_t celsius, fahrenheit;
     
-    celsius = ((adc_read(PINC0) * 5000) / 1024) - 500;
+    celsius = ((calculate_average(PINC0) * 5000) / 1024) - 500;
     itoa(celsius, temperature->celsius, DECIMAL_SYSTEM);
     /* move last digit to right by one and place decimal mark in empty place */
     *(temperature->celsius+3) = *(temperature->celsius+2);
