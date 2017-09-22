4acc9de18c52fce79ed46360c00ff80cb6629bc9
Tommy <tommyy94@users.noreply.github.com>
2017-09-23 00:01:35 +0300

    Update sensors.h

--- sensors.h
+++ sensors.h
@@ -1,7 +1,7 @@
 /******************************************************************************
 *
 * 1. NAME
-*       sensor.h
+*       sensors.h
 *
 * 2. DESCRIPTION
 *
@@ -10,11 +10,16 @@
 #ifndef SENSOR_H_
 #define SENSOR_H_
 
+#ifndef F_CPU
+#define F_CPU 1000000UL
+#endif
+
 /******************************************************************************
 *   HEADER FILES                                                              *
 ******************************************************************************/
 #include <stdint.h>
 #include <stdlib.h>
+#include <util/delay.h>
 #include "adc_lib/adc.h"
 
 /******************************************************************************
@@ -24,19 +29,21 @@
 
 /* Global constants */
 #define DECIMAL_SYSTEM 10
+#define MAX_STRING_LENGTH 5
+#define SAMPLES 100
 
 /* Global variables */
 
 /* Global structures */
-struct sensor
-{
-    char celsius[5];
-    char fahrenheit[5];
+struct sensor {
+    char celsius[MAX_STRING_LENGTH];
+    char fahrenheit[MAX_STRING_LENGTH];
 };
 
 /******************************************************************************
 *   FUNCTION PROTOTYPES                                                       *
 ******************************************************************************/
+void sensor_init(const uint8_t analog_channel);
 void sensor_read(struct sensor *temperature);
 
 #endif /* SENSOR_H_ */
