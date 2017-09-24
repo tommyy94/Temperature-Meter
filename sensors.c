307884d26234156a0008d1a3230bd57cb4047a75
Tommy <tommyy94@users.noreply.github.com>
2017-09-24 23:39:15 +0300

    Update sensors.c

--- sensors.c
+++ sensors.c
@@ -30,7 +30,7 @@ static void calculate_average(uint8_t const analog_channel,
 {
     uint8_t i;
     *average = 0;
-    
+
     for(i = 0; i < SAMPLES; i++) {
         *average += adc_read(analog_channel);
         _delay_ms(10);
@@ -47,7 +47,7 @@ static void calculate_average(uint8_t const analog_channel,
 void sensor_init(uint8_t const analog_channel)
 {
     uint8_t i;
-    
+
     for(i = 0; i < SAMPLES; i++) {
         adc_read(analog_channel);
         _delay_ms(10);
@@ -65,18 +65,17 @@ void sensor_read(struct sensor *temperature)
     char const decimal_mark[] = ".";
     int32_t celsius, fahrenheit, average;
     int32_t *const ptr_average = &average;
-    
+
     calculate_average(PINC0, ptr_average);
-    
-    celsius = ((*ptr_average * 5000) / 1024) - 500;
+
+    celsius = CELSIUS_FORMULA(*ptr_average);
+    fahrenheit = FAHRENHEIT_FORMULA(celsius);
     itoa(celsius, temperature->celsius, DECIMAL_SYSTEM);
-    /* move last digit to right by one and place decimal mark in empty place */
+    itoa(fahrenheit, temperature->fahrenheit, DECIMAL_SYSTEM);
+
+    /* shift a digit and place a decimal mark */
     *(temperature->celsius+3) = *(temperature->celsius+2);
     *(temperature->celsius+2) = *decimal_mark;
-    
-    fahrenheit = (celsius * 9 / 5) + 320;
-    itoa(fahrenheit, temperature->fahrenheit, DECIMAL_SYSTEM);
-    /* again shifting a digit and placing a decimal mark */
     *(temperature->fahrenheit+3) = *(temperature->fahrenheit+2);
     *(temperature->fahrenheit+2) = *decimal_mark;
 }
