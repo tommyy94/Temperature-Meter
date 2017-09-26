cb3dd5ccb716c1b29293874ecd1b1236b9f859fd
Tommy <tommyy94@users.noreply.github.com>
2017-09-26 22:28:36 +0300

    Update sensors.h

--- sensors.h
+++ sensors.h
@@ -20,6 +20,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <util/delay.h>
+#include "main.h"
 #include "adc_lib/adc.h"
 
 /******************************************************************************
@@ -30,10 +31,13 @@
 /* Global constants */
 #define DECIMAL_SYSTEM 10
 #define MAX_STRING_LENGTH 5
-#define SAMPLES 100
+#define SAMPLES 1000
 
 #define CELSIUS_FORMULA(average) (((average) * 5000 / 1024) - 500)
 #define FAHRENHEIT_FORMULA(celsius) (((celsius) * 9 / 5) + 320)
+
+#define DIGIT_CURRENT_POS 2
+#define DIGIT_NEW_POS 3
 
 /* Global variables */
 
@@ -47,6 +51,8 @@ struct sensor {
 *   FUNCTION PROTOTYPES                                                       *
 ******************************************************************************/
 void sensor_init(uint8_t const analog_channel);
-void sensor_read(struct sensor *temperature);
+void sensor_read(struct sensor *temperature,
+                 uint8_t const analog_channel);
+void shift_digit(char *const digit);
 
 #endif /* SENSOR_H_ */
