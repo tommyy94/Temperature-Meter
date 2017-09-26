8efdd7729e041e653b1bcc84ba3c713022ceb3b1
Tommy <tommyy94@users.noreply.github.com>
2017-09-26 22:29:34 +0300

    Update sensors.c

--- sensors.c
+++ sensors.c
@@ -28,12 +28,12 @@ static void calculate_average(uint8_t const analog_channel,
 static void calculate_average(uint8_t const analog_channel,
                               int32_t *const average)
 {
-    uint8_t i;
+    uint16_t i;
     *average = 0;
 
     for(i = 0; i < SAMPLES; i++) {
         *average += adc_read(analog_channel);
-        _delay_ms(10);
+        _delay_us(DELAY);
     }
     *average /= SAMPLES;
 }
@@ -46,11 +46,11 @@ static void calculate_average(uint8_t const analog_channel,
 ******************************************************************************/
 void sensor_init(uint8_t const analog_channel)
 {
-    uint8_t i;
+    uint16_t i;
 
     for(i = 0; i < SAMPLES; i++) {
         adc_read(analog_channel);
-        _delay_ms(10);
+        _delay_us(DELAY);
     }
 }
 
@@ -60,22 +60,30 @@ void sensor_init(uint8_t const analog_channel)
 @brief Read adc values and compute corresponding temperature values.
 @param Pointer to struct sensor
 ******************************************************************************/
-void sensor_read(struct sensor *temperature)
+void sensor_read(struct sensor *temperature,
+                 uint8_t const analog_channel)
 {
-    char const decimal_mark[] = ".";
     int32_t celsius, fahrenheit, average;
     int32_t *const ptr_average = &average;
 
-    calculate_average(PINC0, ptr_average);
+    calculate_average(analog_channel, ptr_average);
 
     celsius = CELSIUS_FORMULA(*ptr_average);
     fahrenheit = FAHRENHEIT_FORMULA(celsius);
     itoa(celsius, temperature->celsius, DECIMAL_SYSTEM);
     itoa(fahrenheit, temperature->fahrenheit, DECIMAL_SYSTEM);
+}
 
-    /* shift a digit and place a decimal mark */
-    *(temperature->celsius+3) = *(temperature->celsius+2);
-    *(temperature->celsius+2) = *decimal_mark;
-    *(temperature->fahrenheit+3) = *(temperature->fahrenheit+2);
-    *(temperature->fahrenheit+2) = *decimal_mark;
+/******************************************************************************
+*   FUNCTION: shift_digit                                                     *
+***************************************************************************//**
+@brief Shift digit to right.
+@param Pointer to first character of the string.
+******************************************************************************/
+void shift_digit(char *const digit)
+{
+    char const decimal_mark[] = ".";
+
+    *(digit + DIGIT_NEW_POS) = *(digit + DIGIT_CURRENT_POS);
+    *(digit + DIGIT_CURRENT_POS) = *decimal_mark;
 }
