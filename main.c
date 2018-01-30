7a519cbaf9ec84fc30c5573000042eb043fdd76c
Tommi <tommyy94@users.noreply.github.com>
2018-01-30 20:52:27 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 *
 * @section  DESCRIPTION
 *   test
-* 
+*
 *******************************************************************************/
 
 /*******************************************************************************
@@ -23,6 +23,7 @@
 *******************************************************************************/
 #include "main.h"
 
+
 /*******************************************************************************
 *   MAIN PROGRAM                                                               *
 *******************************************************************************/
@@ -37,34 +38,43 @@ void main(void)
     /* set clock division factor to 1 */
     CLKPR = (1 << CLKPCE);
     CLKPR &= ~0xFF;
-    
+
     /* init sequence */
     adc_init();
     sensor_init(SENSOR_1_PIN);
-    lcd_init();
-    wdt_init();
-    _delay_ms(DELAY);
-    
+    lcd_init(SET_8_BIT, CLEAR_DISPLAY);
+    sleep_ms(DELAY);
+
     /* initialize lcd screen with text */
     lcd_send_string(SIXTH_COLUMN, FIRST_ROW, DEGREE_SIGN "C");
     lcd_send_string(FOURTEENTH_COLUMN, FIRST_ROW, DEGREE_SIGN "F");
     lcd_send_string(SIXTH_COLUMN, SECOND_ROW, DEGREE_SIGN "K");
-    
+
     while(1) {
         /* read sensor values and save to struct */
         sensor_read(ptr_temperature, SENSOR_1_PIN);
-        
+
         /* handle strings */
         shift_digit(ptr_celsius, 2, 3);
         shift_digit(ptr_fahrenheit, 2, 3);
         shift_digit(ptr_kelvin, 3, 4);
-        
+
         /* print values */
         lcd_send_string(SECOND_COLUMN, FIRST_ROW, ptr_celsius);
         lcd_send_string(TENTH_COLUMN, FIRST_ROW, ptr_fahrenheit);
         lcd_send_string(FIRST_COLUMN, SECOND_ROW, ptr_kelvin);
-        
-        _delay_ms(DELAY);
+
+        sleep_ms(DELAY);
         wdt_reset();
     }
 }
+
+
+#ifdef BOOT_DEBUG
+void get_mcusr(void)
+{
+    mcusr_mirror = MCUSR;
+    MCUSR = 0;
+    wdt_disable();
+}
+#endif
